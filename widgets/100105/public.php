0d57668ee857ff50f251a7f3d1870902bff349f7
Kris Hardy <42982112+krishardy83@users.noreply.github.com>
2019-01-15 08:57:26 -0500

    Update public.php

--- widgets/100105/public.php
+++ widgets/100105/public.php
@@ -25,7 +25,7 @@
                 <img 
                     class="lazyload-img hero-img" 
                     src="<?php echo $folder; ?>img/student-life/handtinytrans.gif"
-                    data-src="<?php echo $folder; ?>img/student-life/student-life-hero-3.jpg" />
+                    data-src="<?php echo $folder; ?>img/student-life/student-life-hero-4.jpg" />
             </picture>
         </div>
 
